1c38723523c9c15d49a5c73347f0ded35b3e6035
karifl <lampi_88@hotmail.com>
2020-05-03 21:44:35 +0300

    Add files via upload

---
+++ iterator/Authors.java
@@ -0,0 +1,9 @@
+package iterator;
+
+public interface Authors {
+
+	public String getFirstName();
+	public String getLastName();
+	public String getBook();
+
+}
---
+++ iterator/Main.java
@@ -0,0 +1,37 @@
+package iterator;
+
+
+import java.util.LinkedList;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		Author go = new Author("George", "Orwell", "1984");
+		Author ah = new Author("Aldous", "Huxley", "Brave New World");
+		Author eap = new Author("Edgar Allan", "Poe", "The Masqurade of the Red Death");
+		Author da = new Author("Durante", "Alighieri", "Divina Commedia");
+		Author pkd = new Author("Phillip K.", "Dick", "Second Variety");
+		
+		LinkedList<Author> books = new LinkedList<Author>();
+		
+		books.add(go);
+		books.add(ah);
+		books.add(eap);
+		books.add(da);
+		books.add(pkd);
+		
+		
+		MyThread yin = new MyThread(books);
+		MyThread yang = new MyThread(books);
+		
+		yin.start();
+		try { Thread.sleep(10);} catch (Exception e) {}
+		yang.start();
+		
+	}
+
+}
