8801efaa6c29521b547034af980841520d3b5f30
karifl <lampi_88@hotmail.com>
2020-05-05 21:14:59 +0300

    Update MyThread.java

--- iteratorC/MyThread.java
+++ iteratorC/MyThread.java
@@ -6,19 +6,21 @@ import java.util.LinkedList;
 
 
 public class MyThread extends Thread {
+		private String name;
 		private LinkedList <Author> lista = new LinkedList <Author>();
-		Iterator<Author> it = lista.iterator();
+		private Iterator<Author> it = lista.iterator();
 		
-	public MyThread(LinkedList <Author> x) {
+	public MyThread(LinkedList <Author> x, String name) {
 		this.lista = x;
 		this.it = this.lista.iterator();
+		this.name = name;
 	}	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while(it.hasNext()) {
 			Author ark = it.next();
-			System.out.println("Author: " +ark.firstName +" " +ark.lastName + ", Book: "+ ark.book);
+			System.out.println("Thread: "+name+" Author: " +ark.firstName +" " +ark.lastName + ", Book: "+ ark.book);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
